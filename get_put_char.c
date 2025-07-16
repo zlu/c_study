9a58f17f0605b2e2ad627bacc79760e46dfb5d58
Zhao Lu <z@zlu.me>
2025-07-16 08:54:57 +0800

    add various counting examples

---
+++ get_put_char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  // int c;
+  printf("\nComparison: %d\n", getchar() != EOF );
+  // printf("EOF: %d\n", EOF);
+  // while ((c = getchar()) != EOF)
+  // {
+  //   putchar(c);
+  //   // printf("\nResult: %d\n", c);
+  // }   
+}
